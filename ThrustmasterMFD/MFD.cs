aa0934db751299f0b5fafaecc33099eab032fbcd
Andre Messier <andremessier@mac.com>
2018-02-14 17:24:52 -0500

    initial commit

---
+++ ThrustmasterMFD/MFD.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HidSharp;
+
+namespace ThrustmasterMFD
+{
+    public class MFD
+    {
+        private HidDevice _device;
+        const int THRUSTMASTER_VENDOR_ID = 0x044F;
+        const int MFD1_PRODUCT_ID = 0xB351;
+        const int MFD2_PRODUCT_ID = 0xB352;
+
+        MFD(int id)
+        {
+            this.Id = id;
+            var loader = new HidDeviceLoader();
+            if (id == 1)
+            {
+                _device = loader.GetDevices(THRUSTMASTER_VENDOR_ID, MFD1_PRODUCT_ID).First();
+                if (_device == null)
+                {
+                    throw new Exception("MFD1 Device not found");
+                }
+
+            }
+            else
+            if (id == 2)
+            {
+                _device = loader.GetDevices(THRUSTMASTER_VENDOR_ID, MFD2_PRODUCT_ID).First();
+                if (_device == null)
+                {
+                    throw new Exception("MFD2 Device not found");
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException("Invalid MFD ID selected");
+            }
+        }
+
+        public int Id
+        {
+            get; set;
+        }
+
+        public byte GetLEDStatus()
+        {
+            using (var stream = _device.Open())
+            {
+                var hidbuffer = new byte[_device.MaxFeatureReportLength];
+                hidbuffer[0] = 0x00;
+                stream.GetFeature(hidbuffer);
+                return hidbuffer[1];
+            }
+        }
+
+        public void SetLEDStatus(bool left, bool right)
+        {
+            using (var stream = _device.Open())
+            {
+                byte mask = 0;
+                if (right)
+                    mask += 1;
+                if (left)
+                    mask += 2;
+                var hidbuffer = new byte[] { 0x00, 0x00, mask };
+                stream.SetFeature(hidbuffer);
+            }
+        }
+
+        public byte GetBrightness()
+        {
+            using (var stream = _device.Open())
+            {
+                var hidbuffer = new byte[_device.MaxFeatureReportLength];
+                hidbuffer[0] = 0x00;
+                stream.GetFeature(hidbuffer);
+                return hidbuffer[2];
+            }
+        }
+
+        public void SetBrightness(byte brightness)
+        {
+            using (var stream = _device.Open())
+            {
+                var hidbuffer = new byte[] { 0x00, 0x01, brightness };
+                stream.SetFeature(hidbuffer);
+            }
+        }
+
+    }
+}
---
+++ ThrustmasterMFDConsoleApp/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HidSharp;
+
+namespace ThrustmasterMFDConsoleApp
+{
+    class Program
+    {
+        static int THRUSTMASTER_VENDOR_ID = 0x044F;
+        static int MFD1_PRODUCT_ID = 0xB351;
+        static int MFD2_PRODUCT_ID = 0xB352;
+
+        static void Main(string[] args)
+        {
+            var loader = new HidDeviceLoader();
+            //var devices = loader.GetDevices(THRUSTMASTER_VENDOR_ID, MFD_PRODUCT_ID);
+            var devices = loader.GetDevices(THRUSTMASTER_VENDOR_ID);
+
+            var deviceMFD1 = loader.GetDevices(THRUSTMASTER_VENDOR_ID, MFD1_PRODUCT_ID).First();
+            var deviceMFD2 = loader.GetDevices(THRUSTMASTER_VENDOR_ID, MFD2_PRODUCT_ID).First();
+
+            if (deviceMFD1 != null)
+            {
+                byte brightness = 0x09;
+                byte ledStatus = 0;
+
+                var device = deviceMFD1;
+
+                SetBrightness(brightness, device);
+                brightness = GetBrightness(device);
+
+                //SetLEDStatus(device, false, true);    // left light off, right light on
+                //SetLEDStatus(device, true, false);   // left light on, right light on
+                //SetLEDStatus(device, true, true);   // left light on, right light on
+                SetLEDStatus(device, false, false);   // left light on, right light on
+                ledStatus = GetLEDStatus(device);
+            }
+
+            Console.Write("Devices: ");
+            Console.WriteLine(devices.Count());
+            Console.WriteLine("Done!");
+        }
+
+        private static byte GetLEDStatus(HidDevice device)
+        {
+            using (var stream = device.Open())
+            {
+                // get status
+                var hidbuffer = new byte[device.MaxFeatureReportLength];
+                hidbuffer[0] = 0x00;
+                stream.GetFeature(hidbuffer);
+                Console.WriteLine("Read Feature Report: {0}", String.Join("-", hidbuffer));
+                return hidbuffer[1];
+            }
+        }
+
+        private static void SetLEDStatus(HidDevice device, bool left, bool right)
+        {
+            using (var stream = device.Open())
+            {                
+                byte mask = 0;
+                if (right)
+                    mask += 1;
+                if (left)
+                    mask += 2;
+                var hidbuffer = new byte[] { 0x00, 0x00, mask };
+                Console.WriteLine("Write Feature Report: {0}", String.Join("-", hidbuffer));
+                stream.SetFeature(hidbuffer);
+            }
+        }
+
+        private static byte GetBrightness(HidDevice device)
+        {
+            using (var stream = device.Open())
+            {
+                // get brightness
+                var hidbuffer = new byte[device.MaxFeatureReportLength];
+                hidbuffer[0] = 0x00;
+                stream.GetFeature(hidbuffer);
+                Console.WriteLine("Read Feature Report: {0}", String.Join("-", hidbuffer));
+                return hidbuffer[2];
+            }
+        }
+
+        private static void SetBrightness(byte brightness, HidDevice device)
+        {
+            using (var stream = device.Open())
+            {
+                // set brightness
+                var hidbuffer = new byte[] { 0x00, 0x01, brightness };
+                Console.WriteLine("Write Feature Report: {0}", String.Join("-", hidbuffer));
+                stream.SetFeature(hidbuffer);
+            }
+        }
+    }
+}
